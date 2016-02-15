1d00537164743bf7d384eedbbb19fdd1f6a89f2b
iAndyDev <perepamanohar@gmail.com>
2016-02-16 01:41:04 +0530

    Post Ad Dummy Screen

--- src/com/sabrentkaro/postad/PostAdActivity.java
+++ src/com/sabrentkaro/postad/PostAdActivity.java
@@ -1,7 +1,15 @@
 package com.sabrentkaro.postad;
 
+import android.os.Bundle;
+
 import com.sabrentkaro.BaseActivity;
+import com.sabrentkaro.R;
 
 public class PostAdActivity extends BaseActivity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addContentLayout(R.layout.post_ad_activity);
+	}
 }
